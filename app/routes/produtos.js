0b29d21ad86f2240b570b2fdf74518a53bbe06cf
Thiago Leite <thiagofmleite@gmail.com>
2018-04-06 13:57:18 -0300

    Entendendo HTTP requests e retornos

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,36 @@
 module.exports = function (app) {
-    app.get("/produtos", function (req, res) {
-
-        var connection = new app.infra.connectionFactory();
+    var listaProdutos = function (req, res) {
+        var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
-
         produtosDAO.lista(function (err, results) {
-            res.render('produtos/lista', { lista: results });
+            res.format({
+                html: function () {
+                    res.render('produtos/lista', { lista: results });
+                },
+                json: function () {
+                    res.json(results);
+                }
+            })
         });
-
         connection.end();
+    }
 
+    // GET Lista
+    app.get('/produtos', listaProdutos);
+
+    // GET Form
+    app.get('/produtos/form', function (req, res) {
+        res.render('produtos/form');
+    });
+
+    // POST Salva
+    app.post('/produtos', function (req, res) {
+        var produto = req.body;
+        console.log(produto);
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function (erro, resultado) {
+            res.redirect('/produtos');
+        });
     });
 }

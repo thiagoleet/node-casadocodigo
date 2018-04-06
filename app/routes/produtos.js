2e1913e9e342c58cfcddcd62ab4e1bb88414035c
Thiago Leite <thiagofmleite@gmail.com>
2018-04-06 13:05:28 -0300

    Definindo classes e criando o DAO

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,12 +1,14 @@
 module.exports = function (app) {
-    app.get('/produtos', function (req, res) {
-        var connection = app.infra.connectionFactory();
-        connection.query('select * from livros', function (err, results) {
-           res.render('produtos/lista', {
-               lista: results
-           });
+    app.get("/produtos", function (req, res) {
+
+        var connection = new app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.lista(function (err, results) {
+            res.render('produtos/lista', { lista: results });
         });
 
         connection.end();
+
     });
 }

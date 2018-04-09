72ad7bc04c39f388c1f76a5ab706474c8b522a56
Thiago Leite <thiagofmleite@gmail.com>
2018-04-09 17:13:42 -0300

    usando o cleardb

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -18,6 +18,17 @@ function createDBConnection() {
             database: 'casadocodigo_nodejs_test'
         });
     }
+
+    if (process.env.NODE_ENV == 'production') {
+        var url = process.env.CLEARDB_DATABASE_URL;
+        var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+        return mysql.createConnection({
+            host: grupos[3],
+            user: grupos[1],
+            password: grupos[2],
+            database: grupos[4]
+        });
+    }
 }
 
 module.exports = function () {

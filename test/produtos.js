a7ad7c44953c288f3c760ba2600e2cc24f87c413
Thiago Leite <thiagofmleite@gmail.com>
2018-04-09 14:11:03 -0300

    Validando inserções com testes

--- test/produtos.js
+++ test/produtos.js
@@ -3,11 +3,22 @@ var request = require('supertest')(express);
 var assert = require('assert');
 
 describe('#ProdutosController', function () {
-    it('#listagem json', function (done) {
+    it('#should return as json', function (done) {
         request.get('/produtos')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, done);
+    });
 
+    it('#should not create a product with invalid date', function (done) {
+        request.post('/produtos')
+            .send({ titulo: '', descricao: 'novo livro' })
+            .expect(400, done);
+    });
+
+    it('#should create a product invalid date', function (done) {
+        request.post('/produtos')
+            .send({ titulo: 'titulo', descricao: 'novo livro', preco: 20.5 })
+            .expect(302, done);
     });
 });

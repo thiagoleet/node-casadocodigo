c32107a8827cf89b28682ffa334dc451caec2711
Thiago Leite <thiagofmleite@gmail.com>
2019-08-29 15:38:05 -0300

    Updated with Course 02 material Fixed MIGRATION MarkoJS deprecated features

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,28 +1,30 @@
-// MarkoJS
 require('marko/node-require').install()
 require('marko/express')
 
-// Express
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-// Middlewares
-app.use('/static', express.static('src/app/public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/estatico', express.static('src/app/public'))
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
 app.use(
   methodOverride(function(req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      let method = req.body._method
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method
       delete req.body._method
       return method
     }
   })
 )
 
-// Routes
-const routes = require('../app/routes/routes')
+const routes = require('../app/routes')
 routes(app)
 
 module.exports = app

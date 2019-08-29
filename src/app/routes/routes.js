2fddda0e2a97fda3a92796c9844659455a9a2c12
Thiago Leite <thiagofmleite@gmail.com>
2019-08-29 12:35:00 -0300

    Added views folder Added MarkoJS as dependency

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -13,15 +13,17 @@ module.exports = app => {
   })
 
   app.get('/livros', function(req, res, next) {
-    res.send(`
-        <html>
-        <head>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1> Livros </h1>
-        </body>
-        </html>
-        `)
+    res.marko(require('../views/livros/lista/lista.marko'), {
+      livros: [
+        {
+          id: 1,
+          title: 'Node Fundamentals'
+        },
+        {
+          id: 2,
+          title: 'Node Advanced'
+        }
+      ]
+    })
   })
 }
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,3 +1,5 @@
+require('marko/node-require').install()
+require('marko/express')
 const express = require('express')
 const app = express()
 const routes = require('../app/routes/routes')
